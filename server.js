4b43ecf70acd8780e7feac10065731843983aacb
Owcollier <owcollier@gmail.com>
2017-12-05 11:27:45 -0500

    imported client framework, have rudimentary spells and detail view working

--- server.js
+++ server.js
@@ -2,9 +2,21 @@
 
 const express = require('express');
 
+const data = require('./spells-seed-data');
+
+console.log(data);
+
 const app = express();
 
 app.use(express.static('public'));
+
+app.get('/api/v1/spells', (req, res) => {
+  res.status(200).json(data);
+});
+
+app.get('/api/v1/spells/:id', (req, res) => {
+  res.json(data[req.params.id]);
+});
 
 if (require.main === module) {
   app.listen(process.env.PORT || 8080, function () {
