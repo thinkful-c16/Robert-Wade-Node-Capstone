69ced20346fabe651840630cbd5266df91f9b908
Owcollier <owcollier@gmail.com>
2017-12-05 13:54:41 -0500

    added mlab url to config.js for DATABASE_URL

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 
 exports.DATABASE_URL = process.env.DATABASE_URL ||
 global.DATABASE_URL ||
-'mongodb://localhost/wizardly-app';
+'mongodb://dev:devpass@ds129966.mlab.com:29966/wizardly-app';
 exports.PORT = process.env.PORT || 8080;
