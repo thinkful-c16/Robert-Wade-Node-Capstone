7e756e0ffb01de27b61217a72ef5c13eb1df8781
Owcollier <owcollier@gmail.com>
2017-12-06 18:17:47 -0500

    cleaned up server.js a bit

--- server.js
+++ server.js
@@ -165,39 +165,25 @@ app.get('/api/v1/wizards/:id/spellbook', (req, res) => {
 
 // add to the spellbook for a specific wizard
 
-app.post('/api/v1/wizards/:id/spellbook', (req, res) => {
+// app.post('/api/v1/wizards/:id/spellbook', (req, res) => {
 
-  Spell
-    .findById(req.body.)
+//   Spell
+//     .findById(req.body.)
 
-    Wizard
-      .findByIdAndUpdate(req.params.id,
-        { $push: {
-          spellBook: {
-            spell_id: ,
-            prepared: false
-          }
-        }
-        });
-});
+//     Wizard
+//       .findByIdAndUpdate(req.params.id,
+//         { $push: {
+//           spellBook: {
+//             spell_id: ,
+//             prepared: false
+//           }
+//         }
+//         });
+// });
 
 // remove spell from spellbook for a specific wizard
 
 //update spell in a spellbook (prepared:true/false)
-
-// create or push endpoint for spellbook
-// app.post('/api/v1/wizards/:id/spellbook', (req, res) => {
-//   Wizard
-//     .findById(req.params.id)
-//     .then(wizard => {
-//       res.status(200).json(wizard.spellBook);
-//     })
-//     .catch(err => {
-//       console.error(err);
-//       res.status(500).json({error: 'Something went wrong'});
-//     });
-// });
-
 
 let server;
 
