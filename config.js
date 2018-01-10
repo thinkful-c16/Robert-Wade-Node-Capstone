74d0afb31faaad4d16b1cad0a4c6fb3f6a181052
Robert Frazier <robert.v.frazier@gmail.com>
2018-01-10 10:02:53 -0500

    Fix indentation in config.js line 4

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 require('dotenv').config();
 exports.DATABASE_URL = process.env.DATABASE_URL ||
-global.DATABASE_URL || 'mongodb://localhost/wizardly-app';
+  global.DATABASE_URL || 'mongodb://localhost/wizardly-app';
 
 exports.TEST_DATABASE_URL = (
   process.env.TEST_DATABASE_URL ||
